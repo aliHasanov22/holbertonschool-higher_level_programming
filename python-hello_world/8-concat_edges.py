a80dac7ec53bea2469ef2594e614e0585c435f9e
root <ali2004hasan2005@gmail.com>
2025-12-23 14:45:58 +0000

    pyt

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[42:61] + str[61:83] + str[83:88] + str[89:95]
+str = str[37:61] + str[61:82] + str[83:88] + str[89:95]
 print(str)

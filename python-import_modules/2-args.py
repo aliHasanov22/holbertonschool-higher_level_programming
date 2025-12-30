b8da16d800d412b17716251f5bf3a87285d3e357
root <ali2004hasan2005@gmail.com>
2025-12-30 17:52:58 +0000

    pyt

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import sys
-a = sys.argv
-if len(a) == 1:
-    print("0 argument.")
-elif len(a) > 1:
-    print("{} argument:".format(len(a)-1))
-    for x in range(1, len(a)):
-        print("{}: {}".format(x, a[x]))
+if __name__ == "__main__":
+    a = sys.argv
+    if len(a) == 1:
+        print("0 argument.")
+    elif len(a) > 1:
+        print("{} argument:".format(len(a)-1))
+        for x in range(1, len(a)):
+            print("{}: {}".format(x, a[x]))

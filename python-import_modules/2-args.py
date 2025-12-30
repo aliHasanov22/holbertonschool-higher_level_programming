e8ea6bd42e7684af506fb47582ddff81b47db287
root <ali2004hasan2005@gmail.com>
2025-12-30 17:46:34 +0000

    pyt

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+a = sys.argv
+if len(a) == 1:
+    print("0 argument.")
+elif len(a) > 1:
+    print("{} argument:".format(len(a)-1))
+    for x in range(1, len(a)):
+        print("{}: {}".format(x, a[x]))

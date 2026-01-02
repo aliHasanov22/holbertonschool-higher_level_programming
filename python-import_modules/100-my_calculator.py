52da31451af0fad374053dc463438730cec26614
root <ali2004hasan2005@gmail.com>
2026-01-02 16:28:30 +0000

    pyt

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -6,18 +6,18 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     a = int(sys.argv[1])
-    t = sys.argv[2]
+    operator = sys.argv[2]
     b = int(sys.argv[3])
-    if t == "+":
+    if operator == "+":
         print("{} + {} = {}".format(a, b, add(a, b)))
 
-    elif t == "-":
+    elif operator == "-":
         print("{} - {} = {}".format(a, b, sub(a, b)))
 
-    elif t == "*":
+    elif operator == "*":
         print("{} * {} = {}".format(a, b, mul(a, b)))
 
-    elif t == "/":
+    elif operator == "/":
         print("{} / {} = {}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")

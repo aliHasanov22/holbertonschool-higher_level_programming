0a9a5a128df9d0608084cd8adb30612374c4d41f
root <ali2004hasan2005@gmail.com>
2026-01-11 22:35:23 +0400

    extra task

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -1,17 +1,18 @@
+#!/usr/bin/python3
 import sys
 
 
 def safe_function(fct, *args):
-    """ Executes a function safely and catches any exceptions.
+    """Executes a function safely and catches any exceptions.
     Args:
         fct: The function to execute.
         *args: Variable length argument list for the function.
     Returns:
-        The result of the function if successful, otherwise None. """
+        The result of the function if successful, otherwise None."""
     try:
         result = fct(*args)
         return result
-    except Exception as e:
-        # Prints the exception message to stderr
-        sys.stderr.write("Exception: {}\n".format(e))
+    except Exception as error:
+        # sys.stderr.write sends the output to the error stream
+        sys.stderr.write("Exception: {}\n".format(error))
         return None

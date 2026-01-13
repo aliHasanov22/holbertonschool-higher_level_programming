376da3a690f4aa85f8765eeab9f5dd3321b59195
root <ali2004hasan2005@gmail.com>
2026-01-13 23:40:48 +0400

    python

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """this is document"""
-    with open(filename, "r", encoding="uft-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")

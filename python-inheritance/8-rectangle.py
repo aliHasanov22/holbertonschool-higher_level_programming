ca236dd6da6619dc34bbd4e922e98f33fc4d8590
root <ali2004hasan2005@gmail.com>
2026-01-13 16:51:06 +0400

    python

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""python class"""
-BaseGeometry = __import__('7-base_geometry.py').BaseGeometry
+"""this is document"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Rectangle subclass"""
+    """this is document"""
+
     def __init__(self, width, height):
-        """Python"""
+        """this is document"""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
+
         self.__width = width
         self.__height = height

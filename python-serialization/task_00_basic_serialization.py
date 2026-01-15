8faa3e30ca62cae11ebaecbb444143ed9495ecb7
root <ali2004hasan2005@gmail.com>
2026-01-15 13:10:14 +0400

    python serialization

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,16 +1,19 @@
 #!?usr/bin/python
 """Python json serialization"""
-import pickle
+import json
 
 
-def serialize_and_save_to_file(data, filename):
-    """Serialization"""
-    with open(filename, "wb") as file:
-        pickle.dump(data, file)
+def serialize_and_save_to_json(data, filename):
+    """Saves data as a human-readable JSON file"""
+    # Note: Use "w" (text mode) instead of "wb"
+    with open(filename, "w") as file:
+        json.dump(data, file, indent=4)
 
 
-def load_and_deserialize(filename):
-    """Deserialization"""
-    with open(filename, "rb") as file:
-        loaddata = pickle.load(file)
-    print(loaddata)
+def load_and_deserialize_json(filename):
+    """Loads data from a JSON file"""
+    # Note: Use "r" (text mode) instead of "rb"
+    with open(filename, "r") as file:
+        data = json.load(file)
+    print(data)
+    return data

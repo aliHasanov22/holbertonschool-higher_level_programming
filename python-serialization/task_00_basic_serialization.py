ea67bb48c956b37d57f24f496e5c447dfbb53930
root <ali2004hasan2005@gmail.com>
2026-01-15 13:13:11 +0400

    python serialization

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,19 +1,26 @@
-#!?usr/bin/python
-"""Python json serialization"""
+#!/usr/bin/env python3
+"""
+Module for basic JSON serialization and deserialization.
+"""
 import json
 
 
-def serialize_and_save_to_json(data, filename):
-    """Saves data as a human-readable JSON file"""
-    # Note: Use "w" (text mode) instead of "wb"
-    with open(filename, "w") as file:
-        json.dump(data, file, indent=4)
+def serialize_and_save_to_file(data, filename):
+    """
+    Serializes a Python dictionary into a JSON file.
+    Args:
+        data (dict): The dictionary to serialize.
+        filename (str): The name of the file to save the JSON data.
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(data, f)
 
 
-def load_and_deserialize_json(filename):
-    """Loads data from a JSON file"""
-    # Note: Use "r" (text mode) instead of "rb"
-    with open(filename, "r") as file:
-        data = json.load(file)
-    print(data)
-    return data
+def load_and_deserialize(filename):
+    """  Loads and deserializes JSON data from a file back into a dictionary.
+    Args:
+        filename (str): The name of the JSON file to read.
+    Returns:
+        dict: The deserialized Python dictionary.  """
+    with open(filename, 'r', encoding='utf-8') as f:
+        return json.load(f)

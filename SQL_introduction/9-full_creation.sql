3917e33bd0312518045c936af803e4e99a05a67d
aliHasanov22 <ali2004hasan2005@gmail.com>
2026-01-20 20:11:20 +0400

    Create 9-full_creation.sql

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+-- sql ful cration
+CREATE TABLE IF NOT EXISTS second_table (
+  id INT,
+  name VARCHAR(256),
+  score INT
+)
+INSERT INTO second_table (id, name, score) VALUE ( 1, “John”, 10), (2, “Alex”, score), (3, “Bob”, 14), (4, “George”, 8);

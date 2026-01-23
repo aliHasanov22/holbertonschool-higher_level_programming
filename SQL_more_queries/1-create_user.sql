ffc38f6c0ef5b8961dcc40f26420f8996f381404
aliHasanov22 <ali2004hasan2005@gmail.com>
2026-01-23 15:01:27 +0400

    Update 1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- create user 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON my_database.* TO 'user_0d_1'@'%';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;

3203ed5361e210d4150838f425e8503e03d3cfc3
root <root@WINDOWS-Q1K4DP0.localdomain>
2026-02-02 16:50:05 +0400

    python networking

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# get link from user
+import sys
+import urllib.request
+
+
+url = sys.argv[1]
+with urllib.request.urlopen(url) as response:
+    request_id = response.headers.get("X-Request-Id")
+print(request_id)

105d52205b970be055626bc99c8882777bb57154
root <root@WINDOWS-Q1K4DP0.localdomain>
2026-02-02 16:54:39 +0400

    python networking

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""get link from user"""
+"""Fetches a URL and displays the X-Request-Id header value"""
 import sys
 import urllib.request
 
 
-url = sys.argv[1]
-with urllib.request.urlopen(url) as response:
-    request_id = response.headers.get("X-Request-Id")
-print(request_id)
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    with urllib.request.urlopen(url) as response:
+        request_id = response.headers.get("X-Request-Id")
+
+    print(request_id)
+

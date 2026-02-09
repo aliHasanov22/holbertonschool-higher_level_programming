7d259de4435f32bb2b9ba44f89d2629a2c76b0de
root <root@WINDOWS-Q1K4DP0.localdomain>
2026-02-09 16:29:12 +0400

    python Server Side Rendering

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Basic HTML template in flask"""
 from flask import Flask, render_template
+import json
 app = Flask(__name__)
-
 @app.route('/')
 def home():
     """" route for home page"""
@@ -15,6 +15,17 @@ def about():
 def contact():
     """ route for contact page """
     return render_template('contact.html')
+@app.route('/items')
+def items():
+    """"route for items page"""
+    try:
+        with open('items.json', 'r') as json_file:
+            items = json.load(json_file)
+            items_list = items.get('items', [])
+    except (FileNotFoundError, json.JSONDecodeError):
+        items_list = []
+    #pass items to template
+    return render_template('items.html', items=items_list)
 
 if __name__ == '__main__':
     app.run(debug=True,port=5000)

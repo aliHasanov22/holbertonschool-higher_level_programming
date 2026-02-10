871f0b09e917120752d77aca2c9e2d2b5bb48538
root <root@WINDOWS-Q1K4DP0.localdomain>
2026-02-10 16:11:44 +0400

    python networking

--- python-network_1/100-github_commits.py
+++ python-network_1/100-github_commits.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Advance task getting multiple github"""
+""" takes 2 arguments in order to solve this challenge"""
 import sys
 import requests
 
@@ -9,16 +9,9 @@ if __name__ == "__main__":
     owner = sys.argv[2]
     url = f"https://api.github.com/repos/{owner}/{repo}/commits"
     params = {"per_page": 10}
-    resp = requests.get(url, params=params)
-    commits = resp.json()
+    r = requests.get(url, params=params)
+    commits = r.json()
     for c in commits[:10]:
         sha = c.get("sha", "")
-        # Preferred: GitHub user display name if available
-        author_name = ""
-        if c.get("author") and c["author"].get("login"):
-            author_name = c["author"]["login"]
-        else:
-            author_name = (c.get("commit", {})
-                            .get("author", {})
-                            .get("name", ""))
-        print(f"{sha}: {author_name}")
+        name = c.get("commit", {}).get("author", {}).get("name", "")
+        print(f"{sha}: {name}")
